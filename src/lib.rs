924d545ed563fba944c4f7c17e0fad8ff55a462b
Omer Shlomovits <omer.shlomovits@gmail.com>
2020-12-31 14:36:28 +0200

    link fixed

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod threshold_bls;
 /// BLS verification should follow the BLS standard:
 /// [https://tools.ietf.org/html/draft-irtf-cfrg-bls-signature-04]
 /// Therefore, it should be possible to use this library ONLY in applications that follow
-/// the standard as well. i.e. Algorand blockchain.
+/// the standard as well. e.g. Algorand [https://github.com/algorand/bls_sigs_ref]
 
 #[derive(Copy, PartialEq, Eq, Clone, Debug)]
 pub enum Error {
